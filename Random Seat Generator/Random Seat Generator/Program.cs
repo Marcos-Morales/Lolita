3bfed07322afc2c9eade537f728b64ef24c5893c
Marcos Morales <marcos0398@yahoo.com>
2019-11-27 00:15:52 -0600

    Created template for random seat generator

---
+++ Random Seat Generator/Random Seat Generator/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Seat_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Created JSON template for project -- Marcos
+
+            //using (HttpClient client = new HttpClient())
+            //{
+            //    var response = client.GetAsync(@"").Result;
+            //    if (response.IsSuccessStatusCode)
+            //    {
+            //        var content = response.Content.ReadAsStringAsync().Result;
+            //        x = JsonConvert.DeserializeObject<Type>(content);
+            //
+            //        var x = JsonConvert.SerializeObject(x);
+            //    }
+            //}
+
+        }
+    }
+}
