cb68aa550fc74a4b9641406cc23cb0ed4c2fa6d1
mora8608 <mora8608@PCB-LAB109-14>
2019-12-01 20:48:07 -0600

    Adding an option to exclude certain chairs

--- Random Seat Generator/Random Seat Generator/MainWindow.xaml.cs
+++ Random Seat Generator/Random Seat Generator/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,62 @@ namespace Random_Seat_Generator
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<int> brokenSeatNumber = new List<int>();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenFileExplorer_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+            var file = dialog.ShowDialog();
+            FileName.Text = dialog.FileName;
+        }
+
+        private void ReadFile_Click(object sender, RoutedEventArgs e)
+        {
+            if(File.Exists(FileName.Text) == true)
+            {
+                var line = File.ReadAllLines(FileName.Text);
+                List<int> random = new List<int>();
+
+                Random Random = new Random();
+
+                for(int i = 0; i < line.Length; i++)
+                {
+                    int number;
+
+                    var column = line[i].Split(',');
+                    string FirstName = column[0];
+                    string LastName = column[1];
+
+                    do
+                    {
+                        number = Random.Next(1, line.Length + 1);
+                    } while (random.Contains(number));
+
+                    SeatList.Items.Add($"{LastName}, {FirstName} - Computer #{number}");
+
+                    random.Add(number);
+                }
+            }
+        }
+
+        private void FileName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void FileName_Copy_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void BrokenSeats_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
